bc7fa4d377b0ea5b2aeda068fa1cf5ba867c168e
Leonid <lenagovorec58@gmail.com>
2023-10-20 19:58:47 +0300

    Card styles

--- src/components/BoardGames.tsx
+++ src/components/BoardGames.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import "./boardGames.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown, faChevronUp, faStar } from "@fortawesome/free-solid-svg-icons";
+import { faApple, faLinux, faWindows, faXbox } from "@fortawesome/free-brands-svg-icons";
 
 export default function BoardGames() {
   return (
@@ -10,6 +11,14 @@ export default function BoardGames() {
       <div className="filters">
         <FilterGroup />
         <FilterGroup />
+      </div>
+      <div className="cardsBoard">
+            <CardGame/>
+            <CardGame/>
+            <CardGame/>
+            <CardGame/>
+            <CardGame/>
+            <CardGame/>
       </div>
     </div>
   );
@@ -28,4 +37,22 @@ function FilterGroup() {
       </select>
     </div>
   );
+}
+
+function CardGame(){
+    return(
+            <div className="cardGame">
+                <div className="photoGame"></div>
+                <div className="iconsSystems">
+                    <FontAwesomeIcon className="iconSystem" icon={faWindows}/>
+                    <FontAwesomeIcon className="iconSystem" icon={faApple} />
+                    <FontAwesomeIcon className="iconSystem" icon={faLinux} />
+                    <FontAwesomeIcon className="iconSystem" icon={faXbox} />
+                    <div className="rating">
+                        <p className="ratingText">9.5</p>
+                        <FontAwesomeIcon className="star" icon={faStar} />
+                    </div>
+                </div>
+            </div>
+    );
 }
